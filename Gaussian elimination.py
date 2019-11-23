c07027127926c9229face43c87d1d202e50dabc0
bell_liang <liang.foreverbell@qq.com>
2019-11-23 22:04:52 +0800

    deepin打开 vscode 编辑过，在求解线性方程组中实现用于对称正定矩阵的平方法和改进的平方法

--- Gaussian elimination.py
+++ Gaussian elimination.py
@@ -5,9 +5,10 @@ Created on Sat Nov 16 19:27:17 2019
 @author: bell liang
 """
 
-# 高斯消元法、直接三角分解法求解线性方程组
+# 高斯消元法、直接三角分解法、平方法、改进的平方法（对称正定矩阵适用）求解线性方程组
 
 import numpy as np
+import math
 
 def caculate_upper_triangle(A):
     count = len(A) - 1
@@ -40,36 +41,81 @@ def caculate_triangular_factorization(A):
     return(A)
 
 def caculate_L_y(A, b):
-    L = np.eye(len(A))
-    for i in range(1, len(L)):
-        L[i, :i] = A[i, :i]
-    y = np.zeros(len(L[:, 0]))
+    y = np.zeros(len(A[:, 0]))
     y[0] = b[0]
-    for i in range(1, len(L)):
-        y[i] = b[i] - L[i, :i].dot(y[:i])
+    for i in range(1, len(A)):
+        y[i] = b[i] - A[i, :i].dot(y[:i])
     return(y)
 
 def caculate_U_x(A, y):
-    for i in range(1, len(A)):
-        A[i, :i] = 0
-    U = A
-    x = np.zeros(len(U[:, 0]))
-    x[-1] = y[-1] / U[-1, -1]
-    for i in range(2, len(U)+1):
-        x[-i] = (y[-i] - U[-i, (-i+1):].dot(x[(-i+1):])) / U[-i, -i]
+    x = np.zeros(len(A[:, 0]))
+    x[-1] = y[-1] / A[-1, -1]
+    for i in range(2, len(A)+1):
+        x[-i] = (y[-i] - A[-i, (-i+1):].dot(x[(-i+1):])) / A[-i, -i]
     return(x)
 
-A = np.array([[1., 1., 1., 6.],
+def caculate_square_root_method(A):
+    A[0, 0] = math.sqrt(A[0, 0])
+    A[1:, 0] = A[1:, 0] / A[0, 0]
+    A[0, 1:] = A[1:, 0]
+    for i in range(1, len(A)-1):
+        A[i, i] = math.sqrt(A[i, i] - np.sum(A[i, :i] ** 2))
+        for j in range(i+1, len(A)):
+            A[j, i] = A[j, i] - A[j, :i].dot(A[:i, i])
+        A[i, i+1:] = A[i+1:, i]
+    A[-1, -1] = math.sqrt(A[-1, -1] - np.sum(A[-1, :-1] ** 2))
+    return(A)
+    
+def caculate_improved_square_root_method(A):
+    A[1:, 0] = A[1:, 0] / A[0, 0]
+    A[0, 1:] = A[1:, 0]
+    for i in range(1, len(A)-1):
+        A[i, i] = A[i, i] - np.sum(A[i, :i] ** 2 * A.diagonal()[:i])
+        t = A[:i, i] * A.diagonal()[:i]
+        for j in range(i+1, len(A)):
+            A[j, i] = (A[j, i] - A[j, :i].dot(t)) / A[i, i]
+        A[i, i+1:] = A[i+1:, i]
+    A[-1, -1] = A[-1, -1] - np.sum(A[-1, :-1] ** 2 * A.diagonal()[:-1])
+    return(A)
+
+def caculate_improved_square_root_method_y(A, b):
+    y = np.zeros(len(A[:, 0]))
+    y[0] = b[0]
+    for i in range(1, len(A)):
+        y[i] = b[i] - A[i, :i].dot(y[:i])
+    return(y)
+
+def caculate_improved_square_root_method_x(A, y):
+    x = np.zeros(len(A[:, 0]))
+    x[-1] = y[-1] / A[-1, -1]
+    for i in range(2, len(A)+1):
+        x[-i] = y[-i] / A[-i, -i] - A[-i, (-i+1):].dot(x[(-i+1):])
+    return(x)
+    
+# page-143-example-2
+A1 = np.array([[1., 1., 1., 6.],
               [0., 4., -1., 5.],
               [2., -2., 1., 1.]])
-A1 = np.array([[1., 2., 3.],
+
+result = caculate_upper_triangle(A1)
+x1 = caculate_x(A1)
+
+# page-153-example-5
+A2 = np.array([[1., 2., 3.],
                [2., 5., 2.],
                [3., 1., 5.]])
-b = np.array([14., 18., 20.])
+b1 = np.array([14., 18., 20.])
 
-result = caculate_upper_triangle(A)
-x = caculate_x(A)
-LU = caculate_triangular_factorization(A1)
-y = caculate_L_y(LU, b)
-x1 = caculate_U_x(LU, y)
-    
+LU = caculate_triangular_factorization(A2)
+y2 = caculate_L_y(LU, b1)
+x2 = caculate_U_x(LU, y2)
+
+# page-177-practice-10
+A3 = np.array([[2., -1., 1.],
+               [-1., -2., 3.],
+               [1., 3., 1.]])
+b3 = np.array([4., 5., 6.])
+
+LDL_T = caculate_improved_square_root_method(A3)
+y3 = caculate_improved_square_root_method_y(LDL_T, b3)
+x3 = caculate_improved_square_root_method_x(LDL_T, y3)
